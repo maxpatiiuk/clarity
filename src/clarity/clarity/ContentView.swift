3d10e567d7f6253e84484d6cb7d97852608aeaa5
morganbergen <morgan@ku.edu>
2022-10-19 03:28:36 -0500

    added

---
+++ src/clarity/clarity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clarity
+//
+//  Created by Morgan Bergen on 10/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
